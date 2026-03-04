f6aff4735859b43c78e6218630c4828b9a276d5c
mysticalg <dhookster@gmail.com>
2026-03-04 23:25:37 +0000

    Use clear-and-type flow for TikTok description fill

--- extension/background.js
+++ extension/background.js
@@ -735,7 +735,7 @@ async function handleCmd(msg) {
               clearEditable(tiktokEl);
               await wait(150);
             }
-            const filled = matches() || setValue(tiktokEl, text);
+            const filled = matches() || await typeTextIntoEditable(tiktokEl, text);
             await wait(120);
             out[rawKey] = Boolean(filled) && matches();
           } else if (currentPlatform === "facebook" && canonicalKey === "description") {
